53c0124c8a6e223c2c85e8f5c06587162b73e22a
Vivek Kumar <vivekkmr527@gmail.com>
2018-08-21 18:01:43 +0530

    Add Update Script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,26 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true }, (err, db) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server');
+	}	
+	console.log('Connected to MongoDB server');
+  
+	db.collection('User').findOneAndUpdate({
+		_id: new ObjectID('5b7bf9aa660bab6284e48e7a')
+	},{
+		$set: {
+			name: 'Vivek'
+		}
+	}, {
+		returnOriginal: false
+	}).then((err, results) => {
+		if(err){
+			return console.log(err);
+		}
+		console.log(results);
+	});
+	
+  //db.close();
+});
